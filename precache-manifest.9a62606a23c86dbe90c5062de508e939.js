9cd8a9b78daf741606272e29b0a831ef7def88bc
Larsy5 <1183510200@qq.com>
2022-05-04 11:49:00 +0800

    Updates

---
+++ precache-manifest.9a62606a23c86dbe90c5062de508e939.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c895126ae223abd3fcd3fc4a17f1ac31",
+    "url": "/my-crypto-zombie/index.html"
+  },
+  {
+    "revision": "da4f92b910307187c309",
+    "url": "/my-crypto-zombie/static/css/main.8406f7ba.chunk.css"
+  },
+  {
+    "revision": "dd214afbe36cb7ee6aab",
+    "url": "/my-crypto-zombie/static/js/2.e6fdbeaa.chunk.js"
+  },
+  {
+    "revision": "503a578284781eca44b55db6dd24cb1b",
+    "url": "/my-crypto-zombie/static/js/2.e6fdbeaa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da4f92b910307187c309",
+    "url": "/my-crypto-zombie/static/js/main.6dda4972.chunk.js"
+  },
+  {
+    "revision": "817c9e11ebfc9dd79683",
+    "url": "/my-crypto-zombie/static/js/runtime-main.3c1a62d7.js"
+  },
+  {
+    "revision": "d2b2559b651e6a50f8d43f206dcdb8e7",
+    "url": "/my-crypto-zombie/static/media/Btn_Register_Normal.d2b2559b.png"
+  },
+  {
+    "revision": "d3684ace4f10c62c9209b45d3bb724c2",
+    "url": "/my-crypto-zombie/static/media/Btn_Register_Over.d3684ace.png"
+  },
+  {
+    "revision": "64a6f16c4f5ef7e189e75dfc0a52a25f",
+    "url": "/my-crypto-zombie/static/media/homecard@2x.64a6f16c.png"
+  },
+  {
+    "revision": "9d0437bbb5e705bad95ac929361faf3e",
+    "url": "/my-crypto-zombie/static/media/index_bg.9d0437bb.png"
+  },
+  {
+    "revision": "41236031fc933b053717ef1760601e36",
+    "url": "/my-crypto-zombie/static/media/start_btn.41236031.png"
+  },
+  {
+    "revision": "2a4a2235f6ed0e838b49a3f1bec4aae1",
+    "url": "/my-crypto-zombie/static/media/tester-bg@2x.2a4a2235.png"
+  },
+  {
+    "revision": "b102b8003aab95865cb7363428273a16",
+    "url": "/my-crypto-zombie/static/media/walls.b102b800.jpg"
+  },
+  {
+    "revision": "08087132d6dcfcd5c5f471880c104712",
+    "url": "/my-crypto-zombie/static/media/zombierun.08087132.png"
+  }
+]);
